aa25d607700e36d7f991688d417464338c122af9
KazuntyLab <kazuaki.korita@kazuntylab.com>
2026-02-14 23:52:56 +0900

    Subscribe Rust

---
+++ subscribe-rust/target/debug/deps/subscribe_rust-f1878f4d25b290ba.d
@@ -0,0 +1,5 @@
+/Users/kazu/Desktop/code/13_server/swipe/subscribe-rust/target/debug/deps/subscribe_rust-f1878f4d25b290ba: src/main.rs
+
+/Users/kazu/Desktop/code/13_server/swipe/subscribe-rust/target/debug/deps/subscribe_rust-f1878f4d25b290ba.d: src/main.rs
+
+src/main.rs:
---
+++ subscribe-rust/target/debug/subscribe-rust.d
@@ -0,0 +1,1 @@
+/Users/kazu/Desktop/code/13_server/swipe/subscribe-rust/target/debug/subscribe-rust: /Users/kazu/Desktop/code/13_server/swipe/subscribe-rust/src/main.rs
