676cf688e11575538ffc0fa2eb346974548471ab
KazuntyLab <kazuaki.korita@kazuntylab.com>
2026-02-14 22:18:50 +0900

    C++ server

---
+++ build/CMakeFiles/SwipeServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SwipeServer.dir/server.cpp.o"
+  "CMakeFiles/SwipeServer.dir/server.cpp.o.d"
+  "SwipeServer"
+  "SwipeServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SwipeServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SwipeServer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SwipeServer.
+# This may be replaced when dependencies are built.
